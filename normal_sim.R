a605f75054422e95f15dd7a8bdf8b0d3bd80e89c
Kurt R. Peters <kurt.r.peters@gmail.com>
2021-11-04 21:20:00 -0400

    normality sim

--- normal_sim.R
+++ normal_sim.R
@@ -12,23 +12,44 @@ d
 
 hist(d$sum)
 
+pad_zeroes_left <- function(x, digits = 2) {
+  x <- as.integer(x)
+  n_digits <- nchar(paste(x))
+  max_digits <- max(digits, max(n_digits, na.rm = TRUE), na.rm = TRUE)
+  zeroes <- 
+    n_digits %>% 
+    purrr::map(~ paste0(rep.int("0", max_digits - .x), collapse = ""))
+  glue::glue("{zeroes}{x}")
+}
 
+samples <- 1000
+n_features <- 20
 set.seed(1234)
 d <- 
-  1:20 %>% 
-  map(~ runif(10000)) %>% 
-  #map(~ round(runif(1000))) %>%   
-  setNames(glue::glue("x{1:length(.)}")) %>% 
+  1:n_features %>% 
+  map(~ rnorm(samples)) %>% 
+  # map(~ rpois(samples, 10)) %>%   
+  # map(~ runif(samples)) %>%   
+  setNames(glue::glue("x{pad_zeroes_left(1:n_features)}")) %>% 
   bind_cols() %>% 
   rowwise() %>% 
   mutate(sum = sum(c_across()), mean = mean(c_across()))
 glimpse(d)  
+
+d %>% 
+  select(starts_with("x")) %>%  
+  pivot_longer(everything()) %>% 
+  ggplot(aes(x = value)) +
+  geom_histogram() +
+  facet_wrap(~ name)
 
 hist(d$mean)
 qqnorm(d$mean)
 qqline(d$mean)
 
 x <- runif(1000)
+x <- rnorm(1000)
+x <- rpois(1000, 10)
 hist(x)
 qqnorm(x)
 qqline(x)
